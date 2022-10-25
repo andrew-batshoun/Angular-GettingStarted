c759476ed78cf904cf0ff6a5c61919654401a4c5
Batshoun <andrew.batshoun@accenturefederal.com>
2022-10-25 16:24:44 -0500

    completed exercise with video, pushing to personal github for reference and refactoring

---
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
+
+@Component({
+  //selector is needed only if it is nested in another component
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
+
+  onBack(): void{
+    this.router.navigate(['/products']);
+  }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2021",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "assets/images/leaf_rake.png"
+    };
+  }
+
+  
+
+}
